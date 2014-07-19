6f9e096ca8500e3ffabb44586ef75e03add911b4
Mahavir Jain <mahavir.jain@mobisys.in>
2014-07-19 15:11:42 +0530

    initial commit

---
+++ IBP/src/com/mobisys/android/ibp/LoginActivity.java
@@ -0,0 +1,14 @@
+package com.mobisys.android.ibp;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+
+public class LoginActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+
+    }
+}
